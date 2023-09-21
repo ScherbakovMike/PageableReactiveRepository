f77ba17d38e1123263d54545f77a589e80ff81c8
mshcherbakov <mshcherbakov@godaddy.com>
2023-09-21 13:28:21 +0300

    Init commit

--- src/main/java/com/example/pageabletest/PageableTestApplication.java
+++ src/main/java/com/example/pageabletest/PageableTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PageableTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PageableTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PageableTestApplication.class, args);
+    }
 
 }
